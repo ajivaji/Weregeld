c2d88df412ff292ce9af5ce5e85492938614899f
Ajivajijaajjerat <dankcoolguy123@gmail.com>
2023-11-17 15:41:23 -0800

    Add doAction function with support for both 1 word and 2 word commands

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include "Character.h"
 
 
-using action = std::function<void(std::string)>;
+using action = std::function<void(std::string, std::vector<std::string>)>;
 
 
 namespace WG {
@@ -24,13 +24,21 @@ public:
 		std::cout << "What is your name?" << std::endl;
 		std::getline(std::cin, userInput);
 		player.setName(userInput);
+		actionActionablesmap = {
+			{"go", {"north", "south", "east", "west"}},
+			{"look", {"north", "south", "east", "west", "around"}},
+			{"take", {"item1", "item2", "item3"}},
+			{"drop", {"item1", "item2", "item3"}},
+			{"inventory", {}},
+			{"quit", {}}
+		};
+
 	}
 
 	std::vector<std::string> getInput(std::string &userInput) {
 		std::vector<std::string> inputArguments;
 		std::cout << "What would you like to do?" << std::endl;
 		std::getline(std::cin, userInput);
-		//create a loop that iterates through the string, extracting each word without spaces into the vector
 		while(userInput.find(' ') != std::string::npos) {
 			inputArguments.push_back(userInput.substr(0, userInput.find(' ')));
 			userInput.erase(0, userInput.find(' ') + 1);
@@ -39,26 +47,39 @@ public:
 		return inputArguments;
 	}
 
+	void doAction(const std::string& inputArg, const std::vector<std::string>& actionableNames) {
+		if (!actionableNames.empty()) {
+			if (std::find(actionableNames.begin(), actionableNames.end(), inputArg) == actionableNames.end()) {
+				std::cout << "You can't do that." << std::endl;
+				return;
+			}
+		}
+		std::cout << "You did it!" << std::endl;
+	}
+
 	void startGame() {
 		std::string userInput;
 		while(userInput != "quit") {
 			std::vector<std::string> inputArguments;
 			inputArguments = getInput(userInput);
-			if(inputArguments.empty() || inputArguments.size() > 2) {
+			if (inputArguments.empty() || inputArguments.size() > 2) {
 				std::cout << "Invalid input. Try again." << std::endl;
 				continue;
 			}
-			if(!(actions.find(inputArguments[0]) != actions.end())) {
-				std::cout << "This action is not available. Try again." << std::endl;
+			std::string actionArgument = std::move(inputArguments[0]);
+			if (!(actionActionablesmap.find(actionArgument) != actionActionablesmap.end())) {
+				std::cout << "This is not a valid action." << std::endl;
 				continue;
 			}
-
-			actions[inputArguments[0]](inputArguments[1]);
+			//actions[actionArgument](inputArguments[1], actionActionablesmap[actionArgument]);
+			doAction(inputArguments[1], actionActionablesmap[actionArgument]);
 		}
 	}
 
 private:
 	Character player;
+	std::vector<Location> locations;
+	std::map<std::string, std::vector<std::string>> actionActionablesmap;
 	std::map<std::string, action> actions;
 };
 
