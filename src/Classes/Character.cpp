28952afea5d6e49aaab3e0727607e26d4343a8b4
Ajivajijaajjerat <dankcoolguy123@gmail.com>
2023-11-24 11:03:48 -0800

    Add inventory stuff for Character

--- src/Classes/Character.cpp
+++ src/Classes/Character.cpp
@@ -2,8 +2,45 @@
 // Created by ajivaji on 11/16/2023.
 //
 
+#include <algorithm>
 #include "Character.h"
 
-void Character::setName(const std::string& name) {
+namespace WG {
+void Character::setName(const std::string &name) {
 	characterName = name;
+}
+
+std::string Character::getName() {
+	return characterName;
+}
+
+std::vector<Item *> Character::getInventoryObjects() {
+	return inventoryObjects;
+}
+
+void Character::addInventoryObject(Item *item) {
+	inventoryObjects.push_back(item);
+}
+
+void Character::removeInventoryObject(Item *item) {
+	for (auto &inventoryObject: inventoryObjects) {
+		if (inventoryObject == item) {
+			inventoryObjects.erase(std::remove(inventoryObjects.begin(), inventoryObjects.end(), item),
+			                       inventoryObjects.end());
+			break;
+		}
+	}
+}
+
+void Character::hasObject(const std::string &objectName, Object* &outObject) {
+	for (const auto &object: inventoryObjects) {
+		auto objectNamelower = object->getObjectName();
+		std::transform(objectNamelower.begin(), objectNamelower.end(), objectNamelower.begin(), ::tolower);
+		if (objectName == objectNamelower) {
+			outObject = object;
+			return;
+		}
+	}
+}
+
 }
--- src/Classes/Character.h
+++ src/Classes/Character.h
@@ -5,11 +5,26 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include "Objects/Object.h"
+#include "Objects/Item.h"
 
+namespace WG {
 class Character {
 public:
 
-	void setName(const std::string& name);
+	void setName(const std::string &name);
+	std::string getName();
+	std::vector<Item *> getInventoryObjects();
+	void addInventoryObject(Item *item);
+	void removeInventoryObject(Item *item);
+	void hasObject(const std::string &objectName, Object *&outObject);
+
 private:
 	std::string characterName;
+	std::vector<Item *> inventoryObjects;
+
+
 };
+
+}
